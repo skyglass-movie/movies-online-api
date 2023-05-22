91452ceea548380d0ff29d53327bd5346c219a87
skyglass <skyglass2001@gmail.com>
2023-05-23 00:24:22 +0200

    started to make changes to movies api

--- src/main/java/skyglass/composer/movie/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package skyglass.composer.movie.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import skyglass.composer.movie.model.Movie;
-
-public interface MovieRepository extends MongoRepository<Movie, String> {
-}
--- src/main/java/skyglass/composer/movie/repository/UserExtraRepository.java
+++
@@ -1,8 +0,0 @@
-package skyglass.composer.movie.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import skyglass.composer.movie.model.UserExtra;
-
-public interface UserExtraRepository extends MongoRepository<UserExtra, String> {
-}
