770aa07c5d83a79157eb1b7fb75dc6da6b91a0bd
skyglass <skyglass2001@gmail.com>
2023-06-15 22:44:55 +0200

    try to fix cors config

--- src/main/java/com/ivanfranchin/moviesapi/security/CorsConfig.java
+++ src/main/java/com/ivanfranchin/moviesapi/security/CorsConfig.java
@@ -9,10 +9,10 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
-@Configuration
+//@Configuration
 public class CorsConfig {
 
-    @Bean
+    //@Bean
     public CorsFilter corsFilter(@Value("${app.cors.allowed-origins}") List<String> allowedOrigins) {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
